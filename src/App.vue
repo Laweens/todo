1afed12f64a2219bc3ed9615073333f8cf108136
Laweens Francois <lfrancois@brooklynsteamcenter.org>
2023-05-11 14:53:43 -0400

    added delete and add button and made it work

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
 <script setup>
 import {ref} from 'vue'
-let todos = ref (['A','B','C'])
+let todos = ref ([])
+let newTodo = ref('')
 
+
+function TodoButton () {
+  todos.value.push(newTodo.value)
+
+  newTodo.value =''
+}
+
+function deleteTodo (index) {
+     todos.value.splice(index, 1)
+  
+
+}
 </script>
 
 <template>
   <h1> My Todo application</h1>
  
+  <ul>
+    
+ <li v-for="(todo, index) in todos">
+  <button @click="deleteTodo(index)">X</button>
+  {{ index + 1 }}
+  {{ todo }}
+
+     
+ </li>
+</ul>
+
+<input v-model="newTodo" @keydown.enter="TodoButton">
+<button @click="TodoButton"> Add Todo</button>
 
   
   
   </template>
+
+<style>
+todo  {
+  background-color: rgb(11, 112, 78);
+}
+
+
+</style>
+
